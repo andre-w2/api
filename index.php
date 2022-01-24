64de2d161ffbde0e49260bd27d28e98671ccd0c3
Andrew <chibisov.a@internet.ru>
2022-01-24 20:59:42 +0500

    Добавил роутер

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
-    require_once __DIR__ .'./core/system/db.php';
-    require_once __DIR__ .'/core/cors.php';
-	
+require_once __DIR__ .'./core/system/db.php';
+require_once __DIR__ .'/core/cors.php';
+require_once __DIR__ .'/core/system/Router.php';
+
+Router::route('/register', function(){
 	$db = getConnection();
-    
-    $url = $_SERVER['REQUEST_URI'];
-	$url = explode('?', $url);
-	$url = $url[0];
- 
-	if(strpos($url, 'register')) {
-		require_once __DIR__ . '/class/register/register.php';
-	}
+	require_once __DIR__ . '/class/register/register.php';
+});
+
+Router::route('/auth', function(){
+	$db = getConnection();
+	require_once __DIR__ . '/class/auth/auth.php';
+});
+
+Router::execute($_SERVER['REQUEST_URI']);
 ?>
